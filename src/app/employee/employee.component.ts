186553cd4dd11302c7284ee85e4b2eb7d35886dd
Santosh Yadav <santosh.yadav198613@gmail.com>
2020-10-24 09:45:33 +0530

    added trackBy changes

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -28,6 +28,8 @@ export class EmployeeComponent implements OnInit {
     salary: 450000
   }
 
+  empList : Employee[] ;
+
   isWeb = true;
 
   data: Employee[] = [
@@ -72,5 +74,71 @@ export class EmployeeComponent implements OnInit {
   trackByFn(i: number, employee: Employee) {
     console.log(employee, i);
     return employee.id;
+  }
+
+  getData(){
+    this.empList = [
+      {
+        id: 1,
+        name: 'Dinesh',
+        age: 24,
+        dob: new Date('10-Jan-2000'),
+        email: 'test@gmail.com',
+        salary: 450000
+      },
+      {
+        id: 2,
+        name: 'Ravi',
+        age: 25,
+        dob: new Date('10-Nov-2000'),
+        email: 'test2@gmail.com',
+        salary: 450000
+      },
+      {
+        id: 3,
+        name: 'Mahendiran',
+        age: 26,
+        dob: new Date('10-Feb-2000'),
+        email: 'test3@gmail.com',
+        salary: 450000
+      }
+    ];
+  }
+
+  getNewData() {
+    this.empList = [
+      {
+        id: 1,
+        name: 'Dinesh',
+        age: 24,
+        dob: new Date('10-Jan-2000'),
+        email: 'test@gmail.com',
+        salary: 450000
+      },
+      {
+        id: 2,
+        name: 'Ravi',
+        age: 25,
+        dob: new Date('10-Nov-2000'),
+        email: 'test2@gmail.com',
+        salary: 450000
+      },
+      {
+        id: 3,
+        name: 'Mahendiran',
+        age: 26,
+        dob: new Date('10-Feb-2000'),
+        email: 'test3@gmail.com',
+        salary: 450000
+      },
+      {
+        id: 4,
+        name: 'Anil',
+        age: 26,
+        dob: new Date('10-Feb-2000'),
+        email: 'test3@gmail.com',
+        salary: 450000
+      }
+    ];
   }
 }
